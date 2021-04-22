59fd641ca207095b3e12a51d69b782e9dd06c26e
Sourav Bose <souravboss.bose@gmail.com>
2021-04-22 22:01:47 +0800

    topic-model update

--- src/textrove/topic_model.py
+++ src/textrove/topic_model.py
@@ -45,6 +45,7 @@ stop3 = [re.sub(r"(\|(.)+)|(\n)", "", x.lower())
 STOP_WORD = list(set(list(stopwords.words("english")) + list(STOPWORDS) + stop1 + stop2 + stop3))
 
 
+
 #Spacy
 # import spacy
 # spacy.load('en')
@@ -114,6 +115,97 @@ class DynTM:
     ########### Prepare texts for Topic-Model ##############
 
     def prepare_text(self,):
+        pass
+
+
+    ################## Optimal Topic Number ##################
+
+    def __jaccard_similarity(self, topic_1, topic_2):
+        """
+        Derives the Jaccard similarity of two topics
+
+        Jaccard similarity:
+        - A statistic used for comparing the similarity and diversity of sample sets
+        - J(A,B) = (A ∩ B)/(A ∪ B)
+        - Goal is low Jaccard scores for coverage of the diverse elements
+        """
+        intersection = set(topic_1).intersection(set(topic_2))
+        union = set(topic_1).union(set(topic_2))
+
+        return float(len(intersection))/float(len(union))
+
+    
+    def __chooseK(self, limit=20, start=2, step=1):
+        """
+        Compute c_v coherence & Model Stability for various number of topics and find optimal one
+        Parameters:
+        ----------
+        limit : Max num of topics
+        start : Least num of topics
+        step  : Step-size
+        Returns:
+        -------
+        optim_model : List of LDA topic models
+        optim_k : optimal number of Topics
+        """
+        coherence_values = []
+        perplexity_values = []
+        num_topics = list(range(start, limit+step+step, step))
+        model_list = {}
+        LDA_topics = {}
+        for i in num_topics:
+            model_list[i] = gensim.models.ldamulticore.LdaMulticore(corpus=self.corpus, id2word=self.dictionary,
+                            num_topics=i, passes=20, alpha='asymmetric', eta='auto', random_state=42, iterations=500,
+                            per_word_topics=True, eval_every=None)
+
+            shown_topics = model_list[i].show_topics(num_topics=i, num_words=20, formatted=False)
+            LDA_topics[i] = [[word[0] for word in topic[1]] for topic in shown_topics]
+            perplexity_values.append(model_list[i].log_perplexity(self.corpus))
+            coherence_values.append(CoherenceModel(model=model_list[i], texts=self.texts, dictionary=self.dictionary,
+                                    coherence='c_v').get_coherence())
+        
+        LDA_stability = {}
+        for i in range(0, len(num_topics)-1):
+            jaccard_sims = []
+            for t1, topic1 in enumerate(LDA_topics[num_topics[i]]):  # pylint: disable=unused-variable
+                sims = []
+                for t2, topic2 in enumerate(LDA_topics[num_topics[i+1]]):  # pylint: disable=unused-variable
+                    sims.append(self.__jaccard_similarity(topic1, topic2))
+                jaccard_sims.append(sims)
+            LDA_stability[num_topics[i]] = jaccard_sims
+
+        mean_stabilities = [np.array(LDA_stability[i]).mean() for i in num_topics[:-1]]
+
+        coh_sta_diffs = [coherence_values[i] - mean_stabilities[i] for i in range(0, len(num_topics)-1)]
+        coh_sta_max = max(coh_sta_diffs)
+        coh_sta_max_idxs = [i for i, j in enumerate(coh_sta_diffs) if j == coh_sta_max]
+        ideal_topic_num_index = coh_sta_max_idxs[0] # choose less topics in case there's more than one max
+        ideal_topic_num = num_topics[ideal_topic_num_index]
+
+        #### Plot for Optimal K ####
+
+        # plt.figure(figsize=(20,10))
+        # ax = sns.lineplot(x=num_topics[:-1], y=mean_stabilities, label='Average Topic Overlap')
+        # ax = sns.lineplot(x=num_topics[:-1], y=coherences, label='Topic Coherence')
+
+        # ax.axvline(x=ideal_topic_num, label='Ideal Number of Topics', color='black')
+        # ax.axvspan(xmin=ideal_topic_num - 1, xmax=ideal_topic_num + 1, alpha=0.5, facecolor='grey')
+
+        # y_max = max(max(mean_stabilities), max(coherences)) + (0.10 * max(max(mean_stabilities), max(coherences)))
+        # ax.set_ylim([0, y_max])
+        # ax.set_xlim([1, num_topics[-1]-1])
+                        
+        # ax.axes.set_title('Model Metrics per Number of Topics', fontsize=25)
+        # ax.set_ylabel('Metric Level', fontsize=20)
+        # ax.set_xlabel('Number of Topics', fontsize=20)
+        # plt.legend(fontsize=20)
+        # plt.show() 
+
+
+
+    def __train_topicmodel(self):
+        pass
+
 
 
     ################## Topic Modelling Formatted output ##################
@@ -434,3 +526,7 @@ class DynTM:
                                '_lda_sta_' + str(timestamp) + '.html')
 
         return vis
+
+
+
+
