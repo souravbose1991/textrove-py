26bc1e12854b91d25c17a323a14fdb94f3343bdc
Sourav Bose <souravboss.bose@gmail.com>
2021-04-26 18:38:11 +0800

    update on Topic Model

--- src/textrove/topic_model.py
+++ src/textrove/topic_model.py
@@ -1,6 +1,7 @@
-# Gensim
 import os
-import pyLDAvis.gensim  # don't skip this
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import pyLDAvis.gensim_models  # don't skip this
 import pyLDAvis
 from nltk import word_tokenize
 import pandas as pd
@@ -11,10 +12,12 @@ from gensim.models import CoherenceModel
 # from gensim.models import ldaseqmodel
 from gensim.corpora import Dictionary, bleicorpus
 from datetime import datetime
+from .ploty_template import plot_title
+from .eda import Documents
+from . import models
 
 global MODEL_PATH
 MODEL_PATH = os.path.abspath(os.path.dirname(models.__file__))
-
 
 
 ################## Dynamic Topic Modelling ##################
@@ -42,7 +45,7 @@ class DynTM:
             else:
                 raise ValueError("Please enter num_topics > 1")
         else:
-            raise TypeError("Only an object of Documents Class can be passed.")ss
+            raise TypeError("Only an object of Documents Class can be passed.")
             
         self.ldamodel = None
         self.model_path = None
@@ -51,7 +54,6 @@ class DynTM:
         self.texts = None
 
     
-
     ########### Prepare texts for Topic-Model ##############
 
     def __prep_texts(self):
@@ -95,6 +97,21 @@ class DynTM:
         union = set(topic_1).union(set(topic_2))
 
         return float(len(intersection))/float(len(union))
+
+    def __plot_chooseK(self, num_topics, mean_stabilities, coherence_values, perplexity_values, optim_k):
+        fig = make_subplots(rows=3, cols=1, specs=[[{}], [{"rowspan": 2}], [None]], shared_xaxes=True, vertical_spacing=0.00)
+        fig.add_trace(go.Scatter(x=num_topics[:-1], y=perplexity_values[:-1], mode='lines+markers',
+                                line_shape='spline', name='Perplexity Score'), row=1, col=1)
+        fig.add_trace(go.Scatter(x=num_topics[:-1], y=coherence_values[:-1], mode='lines+markers',
+                                line_shape='spline', name='Coherence Score'), row=2, col=1)
+        fig.add_trace(go.Scatter(x=num_topics[:-1], y=mean_stabilities, mode='lines+markers',
+                                 line_shape='spline', name='Stability Score'), row=2, col=1)
+        fig.add_shape(type='line', x0=optim_k, y0=0, x1=optim_k,
+                      line=dict(color="MediumPurple", width=3, dash='dashdot'))
+        fig.update_layout(yaxis_visible=False, yaxis_showticklabels=False, xaxis_title='Number of Topics',
+                          yaxis2_visible=False, yaxis2_showticklabels=False, template="plotly_white",
+                          title_text=plot_title("Optimal Topic Analysis"))
+        fig.show()
 
     
     def __chooseK(self, limit=20, start=2, step=1):
@@ -145,23 +162,7 @@ class DynTM:
         optim_k = num_topics[ideal_topic_num_index]
 
         #### Plot for Optimal K ####
-
-        # plt.figure(figsize=(20,10))
-        # ax = sns.lineplot(x=num_topics[:-1], y=mean_stabilities, label='Average Topic Overlap')
-        # ax = sns.lineplot(x=num_topics[:-1], y=coherences, label='Topic Coherence')
-
-        # ax.axvline(x=ideal_topic_num, label='Ideal Number of Topics', color='black')
-        # ax.axvspan(xmin=ideal_topic_num - 1, xmax=ideal_topic_num + 1, alpha=0.5, facecolor='grey')
-
-        # y_max = max(max(mean_stabilities), max(coherences)) + (0.10 * max(max(mean_stabilities), max(coherences)))
-        # ax.set_ylim([0, y_max])
-        # ax.set_xlim([1, num_topics[-1]-1])
-                        
-        # ax.axes.set_title('Model Metrics per Number of Topics', fontsize=25)
-        # ax.set_ylabel('Metric Level', fontsize=20)
-        # ax.set_xlabel('Number of Topics', fontsize=20)
-        # plt.legend(fontsize=20)
-        # plt.show() 
+        self.__plot_chooseK(num_topics, mean_stabilities, coherence_values, perplexity_values, optim_k)
 
         return optim_k
 
@@ -182,10 +183,8 @@ class DynTM:
     def __save_topicmodel(self, dir_name=None, file_name=None):
         if dir_name is None:
             dir_name = MODEL_PATH
-
         if file_name is None:
             file_name = "LDA_Model"
-
         self.model_path = str(dir_name + file_name.strip() + " " + str(datetime.now()))
         self.ldamodel.save(self.model_path + "/model_obj")
 
@@ -207,10 +206,8 @@ class DynTM:
 
         # Visualize the topics
         pyLDAvis.enable_notebook()
-        vis = pyLDAvis.gensim.prepare(self.ldamodel, self.corpus, self.dictionary, sort_topics=False)
-        pyLDAvis.save_html(vis, str(self.company) +
-                            '_lda_sta_' + str(timestamp) + '.html')
-
+        vis = pyLDAvis.gensim_models.prepare(self.ldamodel, self.corpus, self.dictionary, sort_topics=False)
+        pyLDAvis.save_html(vis, self.model_path + "/model_vis.html")
 
 
     ################## Topic Modelling Formatted output ##################
@@ -241,256 +238,7 @@ class DynTM:
         sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
         return sent_topics_df
 
+
     
-
-
-
-
-
-
-    ################## Defining Topic models for sequences with optimal number of Topics ##################
-
-    def compute_coherence_values(self, limit=11, start=4, step=1):
-        """
-        Compute c_v coherence for various number of topics
-        Parameters:
-        ----------
-        limit : Max num of topics
-        start : Least num of topics
-        step  : Step-size
-        Returns:
-        -------
-        model_list : List of LDA topic models (not now)
-        coherence_values : Coherence values corresponding to the LDA model with respective number of topics (not now)
-        optim_k : optimal number of Topics
-        """
-        coherence_values = []
-        perplexity_values = []
-        model_list = []
-        for num_topics in range(start, limit, step):
-            model = gensim.models.ldamulticore.LdaMulticore(corpus=self.corpus,
-                                                            id2word=self.dictionary,
-                                                            num_topics=num_topics,
-                                                            passes=10,
-                                                            alpha='asymmetric',
-                                                            eta='auto',
-                                                            random_state=42,
-                                                            iterations=500,
-                                                            per_word_topics=True,
-                                                            eval_every=None)
-            model_list.append(model)
-            coherencemodel = CoherenceModel(
-                model=model, texts=self.texts, dictionary=self.dictionary, coherence='c_v')
-            coherence_values.append(coherencemodel.get_coherence())
-            perplexity_values.append(model.log_perplexity(self.corpus))
-
-        plot_val = [-1 * i / j for i,
-                    j in zip(coherence_values, perplexity_values)]
-        x = np.array(coherence_values)
-        z = (x-min(x))/(max(x)-min(x))
-        scaled_coherence = z.tolist()
-        scaled_subset = [i for i in scaled_coherence if i >=
-                         max(scaled_coherence)*0.7]
-        scaled_subset_index = [
-            scaled_coherence.index(i) for i in scaled_subset]
-
-        try:
-            scaled_subset_index.remove(4)
-            if(len(scaled_subset_index) == 0):
-                scaled_subset_index = [4]
-        except:
-            scaled_subset_index = scaled_subset_index
-        finally:
-            best_model = model_list[min(scaled_subset_index)]
-
-        optim_k = best_model.get_topics().shape[0]
-
-        x = range(start, limit, step)
-        plt.plot(x, coherence_values)
-        plt.xlabel("Num Topics")
-        plt.ylabel("Coherence score")
-        plt.legend(("coherence_values"), loc='best')
-        plt.show()
-
-        x = range(start, limit, step)
-        plt.plot(x, perplexity_values)
-        plt.xlabel("Num Topics")
-        plt.ylabel("Perplexity score")
-        plt.legend(("perplexity_values"), loc='best')
-        plt.show()
-
-        self.num_topics = optim_k
-
-    ################## Defining Topic models evaluation methods ##################
-
-    def evaluate_model(self, coherence='c_v', seq=False):
-            coherencemodel = CoherenceModel(
-                model=self.model, texts=self.texts, dictionary=self.dictionary, coherence='c_v')
-            top_topics = self.model.top_topics(
-                corpus=self.corpus, texts=self.texts, dictionary=self.dictionary, window_size=None, coherence='c_v', topn=20)
-            # Average topic coherence is the sum of topic coherences of all topics, divided by the number of topics.
-            avg_topic_coherence = sum(
-                [t[1] for t in top_topics]) / len(top_topics)
-            print('Average topic coherence: %.4f.' % avg_topic_coherence)
-            print('Model coherence: %.4f.', coherencemodel.get_coherence())
-            print('Perplexity: ', self.model.log_perplexity(self.corpus))
-
-    ################## Topic Trends for DTM ##################
-
-    def topic_trends(self, explore_topic=1, top_terms=5, normalize=False):
-        result = pd.DataFrame([], columns=['keywords'])
-        for i in range(len(self.uniquetimes)):
-            x = pd.DataFrame(self.model.print_topic(
-                explore_topic-1, time=i, top_terms=top_terms), columns=['keywords', 'weight'])
-            if(normalize == True):
-                x['Nmlz_Prob_' + str(self.uniquetimes[i])
-                  ] = x['weight']/x['weight'].sum()
-            else:
-                x['Nmlz_Prob_' + str(self.uniquetimes[i])] = x['weight']
-            x = x.drop(['weight'], axis=1)
-            result = pd.merge(result, x, on='keywords', how='outer')
-
-        for j in range(len(result)):
-            y = np.array(result.loc[j])[1:]
-            plt.plot(self.uniquetimes, y, label=str(result['keywords'][j]))
-
-        plt.xticks(rotation=70)
-        plt.xlabel("Time Slices")
-        plt.ylabel("Trend Importance")
-        plt.legend()
-        plt.show()
-        return result
-
-    ################## Defining Topic models Training Method ##################
-
-    def dynamic_model(self, n_top=None, timestamp=None, seq=False):
-        """
-        Function to define both Dynamic / Static topic models.
-        Returns:
-        lda_sta_<timestamp>.html = The pyLDA vis html to visualize the static LDA model
-        lda_seq_snap_<timestamp>.html = The pyLDA vis html to visualize the snap at time for the sequential LDA model
-        model_sta.pkl = The pickle object for static LDA model
-        model_seq.pkl = The pickle object for static LDA model
-        Parameters:
-        n_top = (str or int) number of topics (optional) or else the default is 'optimal'
-        timestamp = (str or int) To subset the data by value of 'Year2' column for static LDA and also 
-                    the time-snapshot for LDA vis html in sequential model (for validation) as per values of Year2
-        seq = (bool) To decide if builing a LDA Sequential Model
-        """
-
-        abc1 = self.subset_df
-        if(seq == True):
-            abc1 = abc1.sort_values(by='Year2', ascending=True)
-            abc1.reset_index(drop=True, inplace=True)
-            self.time_df = pd.DataFrame(abc1.Year2, columns=['Year2'])
-            uniquetimes, time_slices = np.unique(
-                abc1.Year2, return_counts=True)
-            self.uniquetimes = uniquetimes
-            self.time_slices = time_slices
-
-            doc_lst = abc1.Cleaned_Text.tolist()
-            doc_lst2 = []
-            doc_lst = [nltk.word_tokenize(str(doc)) for doc in doc_lst]
-
-            # Compute bigrams.
-            # Add bigrams and trigrams to docs (only ones that appear 10 times or more).
-            bigram = Phrases(doc_lst, min_count=2, threshold=1.0)
-            for idx in range(len(doc_lst)):
-                temp_bigram = []
-                for token in bigram[doc_lst[idx]]:
-                    if '_' in token:
-                        # Token is a bigram, add to document.
-                        temp_bigram.append(token)
-                doc_lst.append(temp_bigram)
-
-            # Create Corpus
-            # data_lemmatized = [nltk.word_tokenize(str(sent)) for sent in doc_lst2]
-            dictionary = corpora.Dictionary(doc_lst)
-            dictionary.filter_extremes(no_below=2, no_above=0.8)
-            corpus = [dictionary.doc2bow(text) for text in doc_lst]
-
-            self.texts = doc_lst
-            self.dictionary = dictionary
-            self.corpus = corpus
-
-            if (n_top == 'optimal'):
-                self.compute_coherence_values(limit=11, start=4, step=1)
-
-            # Build LDA Sequence model
-            ldamodel = gensim.models.ldaseqmodel.LdaSeqModel(corpus=self.corpus, time_slice=self.time_slices, id2word=self.dictionary,
-                                                             alphas=0.01, num_topics=self.num_topics, initialize='gensim', sstats=None,
-                                                             lda_model=None, obs_variance=0.5, chain_variance=0.005,
-                                                             passes=10, random_state=42, lda_inference_max_iter=25,
-                                                             em_min_iter=6, em_max_iter=20, chunksize=100)
-
-            self.ldamodel = ldamodel
-            ldamodel.save(str(self.company) + "_model_seq.pkl")
-            self.evaluate_model(coherence='c_v', seq=True)
-            self.topic_trends(explore_topic=1, top_terms=5, normalize=False)
-
-            # Visualize the topics
-            pyLDAvis.enable_notebook()
-
-            if (timestamp is not None):
-                snap = uniquetimes.tolist().index(timestamp)
-#                 print('Coherence at ' + timestamp + ': ', ldamodel.dtm_coherence(snap))
-            else:
-                snap = 0
-
-            doc_topic, topic_term, doc_lengths, term_frequency, vocab = self.ldamodel.dtm_vis(
-                time=snap, corpus=self.corpus)
-            vis = pyLDAvis.prepare(topic_term_dists=topic_term, doc_topic_dists=doc_topic, doc_lengths=doc_lengths, vocab=vocab,
-                                   term_frequency=term_frequency, sort_topics=False)
-            pyLDAvis.save_html(
-                vis, str(self.company) + '_lda_seq_snap_' + str(uniquetimes[snap]) + '.html')
-
-        else:
-            doc_lst = abc1[abc1.Year2 == timestamp].Cleaned_Text.tolist()
-            doc_lst2 = []
-            doc_lst = [nltk.word_tokenize(str(doc)) for doc in doc_lst]
-
-            # Compute bigrams.
-            # Add bigrams and trigrams to docs (only ones that appear 10 times or more).
-            bigram = Phrases(doc_lst, min_count=2, threshold=1.0)
-            for idx in range(len(doc_lst)):
-                temp_bigram = []
-                for token in bigram[doc_lst[idx]]:
-                    if '_' in token:
-                        # Token is a bigram, add to document.
-                        temp_bigram.append(token)
-                doc_lst.append(temp_bigram)
-
-            # Create Corpus
-            # data_lemmatized = [nltk.word_tokenize(str(sent)) for sent in doc_lst2]
-            dictionary = corpora.Dictionary(doc_lst)
-            dictionary.filter_extremes(no_below=2, no_above=0.8)
-            corpus = [dictionary.doc2bow(text) for text in doc_lst]
-
-            self.texts = doc_lst
-            self.dictionary = dictionary
-            self.corpus = corpus
-
-            if (n_top == 'optimal'):
-                self.compute_coherence_values(limit=11, start=4, step=1)
-
-            # Build LDA model
-            ldamodel = gensim.models.ldamulticore.LdaMulticore(corpus=self.corpus, id2word=self.dictionary, num_topics=self.num_topics, passes=10, alpha='asymmetric', eta='auto',
-                                                               random_state=42, iterations=500, per_word_topics=True, eval_every=None)
-
-    #         ldamodel.print_topics()
-            self.ldamodel = ldamodel
-            ldamodel.save(str(self.company) + "_model_sta_" + \
-                          str(timestamp) + ".pkl")
-            self.evaluate_model(coherence='c_v', seq=False)
-
-            # Visualize the topics
-            pyLDAvis.enable_notebook()
-            vis = pyLDAvis.gensim.prepare(ldamodel, corpus, dictionary, sort_topics= False)
-            pyLDAvis.save_html(vis, str(self.company) + \
-                               '_lda_sta_' + str(timestamp) + '.html')
-
-        return vis
-
-
 
 
