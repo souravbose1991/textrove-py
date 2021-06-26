bef8c1d31c6225ed5b79dcd3b983b95a1f06795e
Sourav Bose <souravboss.bose@gmail.com>
2021-06-26 23:23:42 +0800

    spacy-textrank packaged

--- src/textrove/__init__.py
+++ src/textrove/__init__.py
@@ -1,6 +1,5 @@
 from .eda import Documents
 from .sentiment import Sentiment
-# TODO: Create Summarizer based on SPACY
-# from .summarizer import Summary
+from .summarizer import Summary
 from .topic_model import DynTM
 from .word_net import WordNetwork
